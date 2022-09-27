9b11a6bff6e2098cd4896956f4447a0e82414381
preshiousshittu <preciousshittu@outlook.com>
2022-09-27 13:29:50 -0300

    adds comment to explain what method does

--- TEA/src/Main.java
+++ TEA/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
+    //method to encrypt input with key(chance to change datatype as code is implemented)
     public static void encrypt(String input, long key ){
 
     }
+    //method to decrypt input with key(chance to change datatype as code is implemented)
     public static void decrypt(String input, long key ){
 
     }
