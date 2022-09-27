2e68bfa036e9618d25a7dc9645a2147aea097a34
preshiousshittu <preciousshittu@outlook.com>
2022-09-27 13:24:26 -0300

    adds skeleton methods to be filled out later

--- TEA/src/Main.java
+++ TEA/src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    public static void encrypt(String input, long key ){
+
+    }
+    public static void decrypt(String input, long key ){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
