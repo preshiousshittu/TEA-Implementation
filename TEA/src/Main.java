52b7a2ac38c25d0f7d5186edfb0bcd1ebaed0b97
preshiousshittu <preciousshittu@outlook.com>
2022-10-17 18:00:57 -0300

    adds encoding plus encryption test

--- TEA/src/Main.java
+++ TEA/src/Main.java
@@ -1,3 +1,5 @@
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     //method to encrypt input with key(chance to change datatype as code is implemented)
     int delta = 0x9e3779b9;
@@ -5,21 +7,24 @@ public class Main {
     int key[] = new int[]{4, 4, 3 ,6};
     int padding[] = new int[]{5004,38349};
     int L,R;
-    public static  void encrypt(){
+    public static  long encrypt(){
+        long MASK32 = (1l << 32) - 1;
         int delta = 0x9e3779b9;
         int sum = 0;
         int key[] = new int[]{4, 4, 3 ,6};
+        String test = "this is a test";
+        byte[] testBytes = test.getBytes(StandardCharsets.UTF_8);
         int padding[] = new int[]{5004,38349};
         int L,R;
-        L = padding[0] >>> 32;
-        R = padding[1];
+        L = (int) testBytes[0] >>> 32;
+        R = testBytes[1];
 
         for (int i=1; i<=32;i++){
             sum += delta;
             L += ( ((R << 4) + (key[0])) ^ (R + sum) ^ ((R >> 5) + (key[1])));
             R += ( ((L << 4) + (key[2])) ^ (L + sum) ^ ((L >> 5) + (key[3])));
         }
-        System.out.println(Integer.toHexString(L) + Integer.toHexString(R));
+        return L  | R;
     }
     //method to decrypt input with key(chance to change datatype as code is implemented)
     public static void decrypt(String input, long key ){
@@ -27,7 +32,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        encrypt();
+        long n =0;
+        n = encrypt();
+        System.out.println(n);
     }
 }
